6f84bd987bf328ad92b9a5cf6f0beb5cfd3d6bf1
Taictpd00929 <taictpd00929@fpt.edu.vn>
2015-07-01 09:07:35 +0700

    upload

---
+++ Bài_thi_create (2).sql
@@ -0,0 +1,55 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2015-07-01 02:07:26.748
+
+
+
+
+-- tables
+-- Table: TNhanvien
+CREATE TABLE TNhanvien (
+    Ma_NV int  NOT NULL,
+    Ten_NV nvarchar(50)  NOT NULL,
+    Ngay_Sinh datetime  NOT NULL,
+    Gioi_Tinh nvarchar(20)  NOT NULL,
+    Email nvarchar(100)  NOT NULL,
+    Muc_Luong money  NOT NULL,
+    Ma_Phong int  NOT NULL,
+    CONSTRAINT TNhanvien_pk PRIMARY KEY (Ma_NV)
+)
+;
+
+
+
+
+
+-- Table: TPhongBan
+CREATE TABLE TPhongBan (
+    Ma_Phong int  NOT NULL,
+    Ten_Phong nvarchar(50)  NOT NULL,
+    CONSTRAINT TPhongBan_pk PRIMARY KEY (Ma_Phong)
+)
+;
+
+
+
+
+
+
+
+
+
+-- foreign keys
+-- Reference:  TNhanvien_TPhongBan (table: TNhanvien)
+
+
+ALTER TABLE TNhanvien ADD CONSTRAINT TNhanvien_TPhongBan 
+    FOREIGN KEY (Ma_Phong)
+    REFERENCES TPhongBan (Ma_Phong)
+;
+
+
+
+
+
+-- End of file.
+
